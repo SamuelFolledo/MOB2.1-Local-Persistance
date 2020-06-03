62c8b45531143e94093a50ba950c2b79971363e1
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-03 19:01:09 -0400

    [add] init mob classwork

---
+++ classwork/MOB2.1Classwork/MOB2.1Classwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOB2.1Classwork
+//
+//  Created by Macbook Pro 15 on 6/3/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
