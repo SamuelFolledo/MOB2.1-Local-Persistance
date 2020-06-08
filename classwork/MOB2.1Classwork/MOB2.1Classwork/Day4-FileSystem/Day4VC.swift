bbecf3883a365746c55f03c2b3917d2831d418eb
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-08 19:35:52 -0400

    [add] codes from notes

--- classwork/MOB2.1Classwork/MOB2.1Classwork/Day4-FileSystem/Day4VC.swift
+++ classwork/MOB2.1Classwork/MOB2.1Classwork/Day4-FileSystem/Day4VC.swift
@@ -1,5 +1,5 @@
 //
-//  Day3VC.swift
+//  Day4VC.swift
 //  MOB2.1Classwork
 //
 //  Created by Macbook Pro 15 on 6/8/20.
@@ -7,3 +7,48 @@
 //
 
 import UIKit
+
+class Day4VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    fileprivate func setupViews() {
+        view.backgroundColor = .white
+        
+    }
+    
+    func writeToFileSystem() {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory,
+                                            in: .userDomainMask)
+
+        if let documentDirectory: URL = urls.first {
+            let documentURL = documentDirectory.appendingPathComponent("txtFile.txt")
+            // data you want to write to file
+            let data: Data? = "Hello World".data(using: .utf8)
+            do{
+              try data!.write(to: documentURL, options: .atomic) //either write if no error, or dont do it at all
+            }catch{
+                // some error
+            }
+        }
+    }
+    
+    func accessFileInFileSystem() {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        if let documentDirectory: URL = urls.first {
+            let documentURL = documentDirectory.appendingPathComponent("txtFile.txt")
+            do {
+                print(documentURL)
+                let content = try String(contentsOf: documentURL, encoding: .utf8)
+                print(content)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
