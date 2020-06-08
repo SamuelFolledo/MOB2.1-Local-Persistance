55175edc88368a3ab4900bc6eed6eccf9c74b9d0
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-08 19:36:11 -0400

    [add] simple write and access FileSystem function

--- classwork/FileManagerExample.playground/Contents.swift
+++ classwork/FileManagerExample.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
+let anArray : [UInt8] = [226, 143, 179, 226, 156, 136, 239, 184, 143, 240, 159, 165, 179]
+let exampleURL = URL(fileURLWithPath: "message", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("txt") //message.txt file
+let dataFromArray = Data(anArray)
+
+print(exampleURL)
+try dataFromArray.write(to: exampleURL, options: .atomic)
+
+//read
+let savedData = try Data.init(contentsOf: exampleURL)
+print(savedData)
+
+let arrayFromData = Array(savedData)
+print(arrayFromData)
+
+let stringWithMessage = String(data: savedData, encoding: .utf8)
+print(stringWithMessage!)
--- classwork/FileManagerExample.playground/Sources/Extensions/FileManager+extensions.swift
+++ classwork/FileManagerExample.playground/Sources/Extensions/FileManager+extensions.swift
@@ -1,2 +1,7 @@
-import Foundation
+import UIKit
 
+public extension FileManager {
+    static var documentsDirectoryURL: URL = {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }()
+}
