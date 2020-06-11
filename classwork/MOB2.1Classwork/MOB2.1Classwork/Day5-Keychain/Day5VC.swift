9f33bffd94d85741b1863043296ccb22e47c6649
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-11 02:18:54 -0400

    [add] connected outlets an created CRUD for keychainAccess

--- classwork/MOB2.1Classwork/MOB2.1Classwork/Day5-Keychain/Day5VC.swift
+++ classwork/MOB2.1Classwork/MOB2.1Classwork/Day5-Keychain/Day5VC.swift
@@ -7,24 +7,51 @@
 //
 
 import UIKit
+import KeychainAccess
 
 class Day5VC: UIViewController {
-
+    
+    let keychain = Keychain(service: "Mobile2.1App")
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Keychain Access"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func save(key: String = "message", text: String) {
+        keychain[key] = text
     }
-    */
-
+    
+    func reveal(key: String = "message") -> String? {
+        return keychain[key]
+    }
+    
+    func delete(key: String = "message") {
+        do {
+            try keychain.remove(key)
+        } catch let error {
+            print("error: \(error)")
+        }
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        if let text = textField.text, text != "" {
+            save(text: text)
+        } else {
+            print("no text to save")
+        }
+    }
+    
+    @IBAction func revealTapped(_ sender: Any) {
+        let vc = KeychainDetailVC()
+        vc.decryptedMessage = reveal()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func deleteTapped(_ sender: Any) {
+        delete()
+    }
 }
