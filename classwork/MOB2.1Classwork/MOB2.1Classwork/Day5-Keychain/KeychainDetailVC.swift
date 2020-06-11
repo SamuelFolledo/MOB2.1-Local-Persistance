b57b2ac77b1952ae988a58ab24d9de9b8b9c8ff2
Samuel Folledo <samuelfolledo@gmail.com>
2020-06-10 20:34:54 -0400

    [add] day 5 stuffs

---
+++ classwork/MOB2.1Classwork/MOB2.1Classwork/Day5-Keychain/KeychainDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  KeychainDetailVC.swift
+//  MOB2.1Classwork
+//
+//  Created by Samuel Folledo on 6/10/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class KeychainDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
